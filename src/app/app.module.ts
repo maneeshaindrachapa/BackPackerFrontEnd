2aa33f6a5190db2210c9d1aa9e8599a502d0b90c
Maneesha Indrachapa <maneesh.15@cse.mrt.ac.lk>
2018-10-08 11:08:46 +0530

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,28 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { SlideshowModule } from 'ng-simple-slideshow';
+import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
+import { FormsModule } from '@angular/forms';
+import { NouisliderModule } from 'ng2-nouislider';
 
 import { AppComponent } from './app.component';
+import { CarouselComponent } from './components/firstPage/carousel/carousel.component';
+import { NavbarComponent } from './components/firstPage/navbar/navbar.component';
+import { AboutUsComponent } from './components/firstPage/about-us/about-us.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CarouselComponent,
+    NavbarComponent,
+    AboutUsComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    SlideshowModule,
+    FormsModule,
+    NgbModule,
+    NouisliderModule
   ],
   providers: [],
   bootstrap: [AppComponent]
