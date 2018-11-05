14f1fecc64de6380bf0f9d22f656ed0b14346f28
Maneesha Indrachapa <maneesh.15@cse.mrt.ac.lk>
2018-11-05 09:34:32 +0530

    second page edit

--- src/app/components/secondPage/second-page-full/second-page-full.component.ts
+++ src/app/components/secondPage/second-page-full/second-page-full.component.ts
@@ -6,7 +6,6 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./second-page-full.component.css']
 })
 export class SecondPageFullComponent implements OnInit {
-
   constructor() { }
 
   ngOnInit() {
